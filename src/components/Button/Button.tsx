a6f05c1e09237afb421667d5e4b4cbf73727e499
Sumana <sumana.sattar@gmail.com>
2022-04-27 11:26:51 +0200

    refactoring

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,9 +4,9 @@ function Button({ next, previous, goToSlide, ...rest }: any) {
     return (
         <>
             <div className="carousel-button-group mx-auto position-absolute" style={{ right: '50px' }}>
-                <button className={currentSlide === 0 ? 'disable' : ''} onClick={() => previous()} style={{ transform: 'rotate(180deg)', border: 'none', background: 'none' }} ><Image src={require('../../../docs/chevron-circled.svg')} alt='' />
+                <button className={currentSlide === 0 ? 'disable' : ''} onClick={() => previous()} style={{ transform: 'rotate(180deg)', border: 'none', background: 'none' }} ><Image src={require('../../../docs/chevron-circled.svg')} alt='previous button' />
                 </button>
-                <button onClick={() => next()} style={{ position: 'relative', border: 'none', background: 'none' }} ><Image src={require('../../../docs/chevron-circled.svg')} alt='' /></button>
+                <button onClick={() => next()} style={{ position: 'relative', border: 'none', background: 'none' }} ><Image src={require('../../../docs/chevron-circled.svg')} alt='next button' /></button>
             </div>
         </>
     );
